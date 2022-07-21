31da10a21e20d9a13590c0e2b5d590784ef40693
Hitesh Agarwal <hitesh.agarwal@timesinternet.in>
2022-07-22 00:21:52 +0530

    Remove applicationWillEnterForeground function

--- Example/HASlider/AppDelegate.swift
+++ Example/HASlider/AppDelegate.swift
@@ -18,13 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
-
-   
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
+ 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }

3c0b429d57819b40a9072cc5a9fef51e6c6f7457
Hitesh Agarwal <hitesh.agarwal@timesinternet.in>
2022-07-22 00:22:47 +0530

    Remove applicationDidBecomeActive

--- Example/HASlider/AppDelegate.swift
+++ Example/HASlider/AppDelegate.swift
@@ -18,11 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
- 
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
+  
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }

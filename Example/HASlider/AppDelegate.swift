2c989ecf033afd2bcce06d4275b098c2b2b4c698
Hitesh Agarwal <hitesh.agarwal@timesinternet.in>
2022-07-22 16:30:16 +0530

    changes in appDelegate

--- Example/HASlider/AppDelegate.swift
+++ Example/HASlider/AppDelegate.swift
@@ -19,10 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
  
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
+  
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
